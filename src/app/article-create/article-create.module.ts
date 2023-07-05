fb1c6a3d5dbb08d712644ee0498299f341b40840
David Gasparyan <david_gasparyang@gmail.com>
2023-07-05 19:16:41 +0400

    feat: added routing from create article to articles and vice versa

--- src/app/article-create/article-create.module.ts
+++ src/app/article-create/article-create.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ArticleCreateComponent } from './article-create.component';
 import {ReactiveFormsModule} from "@angular/forms";
+import {RouterLink} from "@angular/router";
 
 
 @NgModule({
@@ -10,7 +11,8 @@ import {ReactiveFormsModule} from "@angular/forms";
   ],
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterLink
   ],
   exports: [
     ArticleCreateComponent,
