78405f3cf37cce1809f7a82d89d6fadec51a5a0d
David Gasparyan <david_gasparyang@gmail.com>
2023-07-05 18:01:19 +0400

    feat: added angular app with articles lazy routing

---
+++ src/app/article-list/article-list.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {ArticleListComponent} from "./article-list.component";
+import {ArticleListRoutingModule} from "./article-list-routing.module";
+
+
+@NgModule({
+  declarations: [
+    ArticleListComponent,
+  ],
+  imports: [
+    CommonModule,
+    ArticleListRoutingModule,
+  ],
+})
+export class ArticleListModule { }
